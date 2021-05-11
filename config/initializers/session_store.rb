2ea5435058f87036e051d69a299637cb7deee437
candland <candland@gmail.com>
2021-05-11 11:07:56 -0600

    Remove sidekiq web session store. no longer needed.

--- config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-require "sidekiq/web"
-
-# Turn off Sinatra's sessions, which overwrite the main Rails app's session
-# after the first request
-
-Sidekiq::Web.set :session_secret, Rails.application.credentials[:secret_key_base]
